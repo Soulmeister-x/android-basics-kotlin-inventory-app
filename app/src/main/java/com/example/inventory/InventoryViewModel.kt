690b01d96b7c44acd2a892c23dcc4cdaa4b49d3d
Soul <51956374+Soulmeister-x@users.noreply.github.com>
2022-12-22 14:52:53 +0100

    items can now be displayed in app

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -1,14 +1,14 @@
 package com.example.inventory
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.inventory.data.Item
 import com.example.inventory.data.ItemDao
 import kotlinx.coroutines.launch
 
 
 class InventoryViewModel(private val itemDao: ItemDao): ViewModel() {
+
+    val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
 
     // SQL method to add item to database
     private fun insertItem(item: Item) {
--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.NumberFormat
 
 @Entity(tableName = "item")
 data class Item(
@@ -16,3 +17,9 @@ data class Item(
     @ColumnInfo(name = "quantity")
     val quantityInStock: Int
 )
+
+
+fun Item.getFormattedPrice(): String = NumberFormat.getCurrencyInstance().format(itemPrice)
+
+
+
